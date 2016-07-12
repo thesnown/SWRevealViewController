15ba6946bc7b611952358c9830d05120b6449613
Nick Koslowski <nko@hup.de>
2016-07-12 12:32:26 +0200

    add carthage support
    Adding a Framework Project to enable Carthage use

---
+++ SWRevealViewControllerFramework/SWRevealViewControllerFramework/SWRevealViewControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  SWRevealViewControllerFramework.h
+//  SWRevealViewControllerFramework
+//
+//  Created by Nick Koslowski on 12/07/16.
+//  Copyright © 2016 theSnowN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWRevealViewControllerFramework.
+FOUNDATION_EXPORT double SWRevealViewControllerFrameworkVersionNumber;
+
+//! Project version string for SWRevealViewControllerFramework.
+FOUNDATION_EXPORT const unsigned char SWRevealViewControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWRevealViewControllerFramework/PublicHeader.h>
+
+#import <SWRevealViewControllerFramework/SWRevealViewController.h>
