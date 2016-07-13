191216d85118ba5af111a66f2d23562ddd99bdf2
Nick Koslowski <nko@hup.de>
2016-07-13 15:11:50 +0200

    Fix Conflicting return type in implementation of supportedInterfaceOrientations
    Fixing the following warning
    
    Conflicting return type in implementation of
    'supportedInterfaceOrientations': 'UIInterfaceOrientationMask' (aka
    'enum UIInterfaceOrientationMask') vs 'NSUInteger' (aka 'unsigned int')
    
    #if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
    - (NSUInteger)supportedInterfaceOrientations
    #else
    - (UIInterfaceOrientationMask)supportedInterfaceOrientations
    #endif

--- SWRevealViewControllerFramework/SWRevealViewControllerFramework/SWRevealViewController.m
+++ SWRevealViewControllerFramework/SWRevealViewControllerFramework/SWRevealViewController.m
@@ -760,9 +760,11 @@ const int FrontViewPositionNone = 0xff;
     // intercepting it on the panGestureBegan and panGestureEnded delegates
     _userInteractionStore = _contentView.userInteractionEnabled;
 }
-
-
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
 - (NSUInteger)supportedInterfaceOrientations
+#else
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+#endif
 {
     // we could have simply not implemented this, but we choose to call super to make explicit that we
     // want the default behavior.
